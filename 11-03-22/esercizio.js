620fb38264410de600a600715bc1a46bb169ea0f
Bluealastor <94492588+Bluealastor@users.noreply.github.com>
2022-03-11 13:55:23 +0100

    Add files via upload

---
+++ 11-03-22/esercizio.js
@@ -0,0 +1,40 @@
+/*
+ESERCIZI SULLO SCOPE ATTENZIONE: prova a prima dare prima la risposta e solo dopo testare il codice!! 
+Giustificatene il motivo con un commento, subito accanto a 'RISPOSTA:'. */
+
+// Es. 1: Dato il seguente 'snippet' (piccolo programmino), qual'è il risultato dei console.log? 
+// RISPOSTA:  syntax error (cost PI errata sintassi )
+
+// cost PI = 3, 14;
+
+// console.log(PI);
+
+// se(PI >= 3) { console.log(PI); }
+
+// Es. 2: Dato il seguente, qual'è il risultato della console.log?
+// RISPOSTA: syntax error (lascia che mesiInAAanno: sintassi errata)
+
+// var favColor = "viola";
+// lascia che mesiInAAanno = 12;
+
+// if (true) {
+//     var favColor = "viola";
+//     console.log("Il mio colore preferito è:", favColor);
+// }
+
+// console.log("Ci sono", mesiInAYear, "mesi in un anno.");
+
+// // Es. 3: Dato il seguente, qual'è il risultato dei console.log?
+// // RISPOSTA: syntax error (parestesi graffa finale di function  mancante )
+
+// function makePizza(moreIngredients) {
+//     let theSecond = "Sugo di pomodoro freschissimo";
+//     // { ... } console.log(theSecond + ", " + moreIngredients); restituisce vero; 
+//     // non fa niente in pratica! Ritorna solo true (ne riparleremo a lezione!) }
+
+//     if (true) {
+//         makePizza("rucola, prosciutto crudo e scaglie di grana.");
+
+//         console.log("Il secondo ingrediente necessario: ", makePizza);
+//     }
+// }
