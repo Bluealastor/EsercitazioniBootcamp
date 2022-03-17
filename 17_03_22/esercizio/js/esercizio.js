ab92dd6d309fb4a5c39dbb31d6e33e0a03fd745d
Christian <94492588+Bluealastor@users.noreply.github.com>
2022-03-17 17:46:14 +0100

    Add files via upload

---
+++ 17_03_22/esercizio/js/esercizio.js
@@ -0,0 +1,85 @@
+/*
+Dato un array di film:
+
+const movies = [
+  "Deadpool",
+  "Dune",
+  "Grosso guaio a Chinatown",
+  "V per vendetta",
+  "Pulp Fiction",
+  "Signore degli anelli",
+  "Scarface",
+  "Sharknado (1,2,3,4,5 e 6)",
+  "Blade Runner",
+];
+
+    "stampare" questi in pagina in una lista HTML
+    aggiungere una input di testo e un button sopra la lista, quando clicco il button la lista nella pagina dovrà essere filtrata in base al testo scritto nella input
+    [opzionalmente] stilare il tutto
+
+*/
+
+
+
+
+const movies = [
+    "Deadpool",
+    "Dune",
+    "Grosso guaio a Chinatown",
+    "V per vendetta",
+    "Pulp Fiction",
+    "Signore degli anelli",
+    "Scarface",
+    "Sharknado (1,2,3,4,5 e 6)",
+    "Blade Runner",
+];
+
+const listFilm = document.querySelector("#listaFilm");
+const submit = document.querySelector("#submit");
+const add = document.querySelector("#add")
+
+
+// const film = () => {
+
+//     const list = movies.filter((element) =>
+//             element.toLowerCase().includes(add.value))
+//         .join("")
+//     listFilm.innerHTML = list;
+// }
+
+submit.addEventListener("click", () => {
+    const search = movies.filter((element) =>
+            element.toLowerCase().includes(add.value))
+        // filmSearch.innerHTML = search;
+
+    listFilm.innerHTML = search + movies.map((e) => {
+        return `
+    <hr>
+    <li>${e}</li>
+                       `;
+    }).join(" ");
+
+
+});
+
+
+
+
+
+
+
+
+// const stampa = () => {
+
+
+//     listFilm.innerHTML = list.map((film) => {
+//         `
+// <li>
+// ${list}
+// </li>            
+//             `
+
+//     }).join("");
+// };
+
+// stampa()
