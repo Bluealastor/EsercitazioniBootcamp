c01b945348a82be5c9fce121466d1090650d5d4e
Christian <94492588+Bluealastor@users.noreply.github.com>
2022-03-17 18:16:14 +0100

    s

--- 17_03_22/esercizio/js/esercizio.js
+++ 17_03_22/esercizio/js/esercizio.js
@@ -1,18 +1,6 @@
 /*
 Dato un array di film:
 
-const movies = [
-  "Deadpool",
-  "Dune",
-  "Grosso guaio a Chinatown",
-  "V per vendetta",
-  "Pulp Fiction",
-  "Signore degli anelli",
-  "Scarface",
-  "Sharknado (1,2,3,4,5 e 6)",
-  "Blade Runner",
-];
-
     "stampare" questi in pagina in una lista HTML
     aggiungere una input di testo e un button sopra la lista, quando clicco il button la lista nella pagina dovrà essere filtrata in base al testo scritto nella input
     [opzionalmente] stilare il tutto
@@ -36,50 +24,27 @@ const movies = [
 
 const listFilm = document.querySelector("#listaFilm");
 const submit = document.querySelector("#submit");
-const add = document.querySelector("#add")
+const add = document.querySelector("#add");
 
 
-// const film = () => {
+const film = () => {
 
-//     const list = movies.filter((element) =>
-//             element.toLowerCase().includes(add.value))
-//         .join("")
-//     listFilm.innerHTML = list;
-// }
+    const list = movies.filter((element) =>
+            element.toLowerCase().includes(add.value))
+        .join("")
+    listFilm.innerHTML = movies.map((e) => {
+        return `<li>${e}</li>`
+    }).join(" ");;
+}
+
+film()
 
 submit.addEventListener("click", () => {
     const search = movies.filter((element) =>
-            element.toLowerCase().includes(add.value))
-        // filmSearch.innerHTML = search;
+        element.toLowerCase().includes(add.value));
+    // filmSearch.innerHTML = search;
 
-    listFilm.innerHTML = search + movies.map((e) => {
-        return `
-    <hr>
-    <li>${e}</li>
-                       `;
-    }).join(" ");
+    listFilm.innerHTML = search;
 
 
 });
-
-
-
-
-
-
-
-
-// const stampa = () => {
-
-
-//     listFilm.innerHTML = list.map((film) => {
-//         `
-// <li>
-// ${list}
-// </li>            
-//             `
-
-//     }).join("");
-// };
-
-// stampa()
